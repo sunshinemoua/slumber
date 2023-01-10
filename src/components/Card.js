cca0db9680dca8a9cff70497716399d478337464
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-01-09 17:24:01 -0800

    added sleep and wake date/time inputs

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,6 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = ({ children, ...props }) => {
-  console.log(children);
   return (
     <div className={classes.card} {...props}>
       {children}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,25 +1,13 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import Card from "./Card";
+import Navbar from "../redux/navbar";
+import Stopwatch from "./Stopwatch";
 import classes from "./Welcome.module.css";
 
 const Welcome = () => {
   return (
     <div className={classes.page}>
-      <h2 className={classes.title}>Welcome to Slumber</h2>
-      <div className={classes.link}>
-        <NavLink className={classes.link} to="/About">
-          About
-        </NavLink>
-        <NavLink className={classes.link} to="/Dashboard">
-          Dashboard{" "}
-        </NavLink>
-        <NavLink className={classes.link} to="/FAQs">
-          FAQs
-        </NavLink>
-      </div>
-
-      <Card className={classes.intro}> TESTING TESTING</Card>
+      <Navbar />
+      <Stopwatch />
     </div>
   );
 };
